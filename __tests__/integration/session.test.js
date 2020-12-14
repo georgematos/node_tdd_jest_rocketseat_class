85137f7fe91164847af3aa36c66871c2890c0d99
George Matos <georgemattos@gmail.com>
2020-12-14 17:27:59 -0300

    test: add User sequelize model

---
+++ __tests__/integration/session.test.js
@@ -0,0 +1,27 @@
+const request = require('supertest')
+const app = require('../../src/app')
+const { User } = require('../../src/app/models')
+const truncate = require('../utils/truncate')
+
+describe('Authentication', () => {
+  beforeEach(async () => {
+    await truncate()
+  })
+
+  it('should authenticate with valid credentials', async () => {
+    const user = await User.create({
+      name: "George",
+      email: 'georgemattos@gmail.com',
+      password: '123123'
+    })
+
+    const response = await request(app)
+      .post('/sessions')
+      .send({
+        email: user.email,
+        password: '123123'
+      })
+
+      expect(response.status).toBe(200)
+  })
+})
